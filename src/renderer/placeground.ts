a5309afd42e4faa509cc740dc03769c3ebf75bad
bethkoulyras <43272226+bethkoulyras@users.noreply.github.com>
2024-11-13 11:22:27 +1100

    update: models

--- src/renderer/placeground.ts
+++ src/renderer/placeground.ts
@@ -206,8 +206,8 @@ export function init3dExperience(
               material instanceof MeshStandardMaterial ||
               material instanceof MeshPhysicalMaterial
             ) {
-              material.roughness = 0.35;
-              material.metalness = 0.7;
+              material.roughness = 0.2;
+              material.metalness = 0.9;
               material.needsUpdate = true;
             }
           }
